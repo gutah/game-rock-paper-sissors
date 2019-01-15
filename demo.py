5ee79bcc067951d3822983498bec761a179ff04d
Maciel <748302@cognizant.com>
2019-01-15 15:10:38 -0200

    beginning of the project

---
+++ demo.py
@@ -0,0 +1,21 @@
+#Perguntar os dados de dois usuários
+#Usar while
+#Rock, Paper, Scissors
+#rand.int(1,3)
+
+def compare(u1, u2):
+    
+    if u1 == u2:
+        return "Empate"
+    elif u1 == "Papel"
+        if u2 == "Tesoura"
+            print("Tesoura ganha.")
+        
+    
+
+player_1 = input("What's your name?")
+player_2 = "Computer"
+while True:
+    u1 = int(input('Choose:\n 1)Rock\n 2)Paper\n 3)Sissors'))
+    u2 = rand.int(1,3)
+    print(compare(u1, u2))
