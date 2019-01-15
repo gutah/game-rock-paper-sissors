f97be90817aca5e5b6ed6e9f175bdd82d99fce78
Gustavo Maciel <g-maciel@outlook.com>
2019-01-15 16:10:00 -0200

    Fix some bug and code errors

--- demo.py
+++ demo.py
@@ -13,31 +13,31 @@ Description: Python study through Rock, paper and scissor game.
 def compare(u1, u2):
     
     if u1 == u2:
-        return "It's a Tie!"
+        print("It's a Tie!")
     
-    elif u1 == "paper":
-        if u2 == "Scissors"
+    elif u1 == "paper" or 2:
+        if u2 == "Scissors" or 3:
             print("Sissor Win!")
         else:
             print("Paper Win!")
     
-    elif u1 == "rock":
-        if u2 == "Scissor"
+    elif u1 == "rock" or 1:
+        if u2 == "Scissor" or 3:
             print("Scissor Win!")
         else:
             else("Paper win!")
     
-    elif u1 == "scissor":
-        if u2 == "paper":
+    elif u1 == "scissor" or 3:
+        if u2 == "paper" or 2:
             print("Paper win!")
         else:
             print("Rock win!")
     
 
-player_1 = input("What's your name?")
-player_2 = "Computer"
+player_1 = input("Player 1: What's your name?")
+player_2 = input("Player 2: What's your name?")
 
 while True:
-    u1 = input('Choose:\n 1)Rock\n 2)Paper\n 3)Scissors').lower()
-    u2 = input('Choose:\n 1)Rock\n 2)Paper\n 3)Scissors').lower()
+    u1 = input('Choose:\n 1->Rock\n 2->Paper\n 3->Scissors').lower()
+    u2 = input('Choose:\n 1->Rock\n 2->Paper\n 3->Scissors').lower()
     compare(u1, u2)
